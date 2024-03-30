1f031f40e6cdeecbe1bed0fa4f341cf9074c20ad
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-03-29 21:45:30 -0300

    feat: added home page

---
+++ lib/src/home_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:moedapp/src/constants/app_theme.dart';
+import 'package:moedapp/src/home_bottom_navigation_bar_view.dart';
+import 'package:provider/provider.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  static const routeName = '/home';
+
+  @override
+  Widget build(BuildContext context) {
+    final int selectedIndex =
+        Provider.of<NavigationControllerProvider>(context).selectedIndex;
+
+    return Scaffold(
+      backgroundColor: AppTheme.appBackgroundColor,
+      resizeToAvoidBottomInset: true,
+      bottomNavigationBar: const HomeBottomNavigationBarView(),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 32.0,
+            right: 32.0,
+            top: 8.0,
+          ),
+          child: getPage(selectedIndex),
+        ),
+      ),
+    );
+  }
+}
