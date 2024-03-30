c63f634a5581862a05421dc95335bb0d0edd2d68
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-03-30 15:36:20 -0300

    feat: added method to get latest currency

---
+++ lib/src/services/currency_converter_service.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:moedapp/src/api.dart';
+import 'package:moedapp/src/constants/api_routes.dart';
+import 'package:moedapp/src/models/latest_model.dart';
+
+/// This service use `BRL` currency by default
+/// but you could implement api requests to ip APIs
+class CurrencyConverterService {
+  final ApiClient _api = ApiClient();
+
+  Future<LatestModel> getLatestCurrency({
+    String? fromCurrency,
+    String? toCurrency,
+  }) async {
+    try {
+      final response = await _api.get(Uri.parse(ApiRoutes.getLatestCurrency(
+        fromCurrency: fromCurrency,
+        toCurrency: toCurrency,
+      )));
+      if (response.statusCode == 200) {
+        return LatestModel.fromJson(jsonDecode(response.body));
+      } else if (response.statusCode == 404) {
+        throw Exception("URL não encontrada");
+      } else {
+        throw Exception("Erro ao buscar informações");
+      }
+    } on SocketException catch (_) {
+      throw "Sem conexão com a internet, tente novamente mais tarde";
+    } catch (e) {
+      throw "Ocorreu um erro desconhecido, entre em contato com o suporte";
+    }
+  }
+}
