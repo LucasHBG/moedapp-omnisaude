9d01a572eda1fd418fcf506d242e396a7b28d50a
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-03-30 15:35:30 -0300

    feat: added currency service

--- lib/src/currency_converter_view.dart
+++ lib/src/currency_converter_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moedapp/src/common_widgets/loading_spinner_widget.dart';
 import 'package:moedapp/src/constants/app_theme.dart';
+import 'package:moedapp/src/services/currency_converter_service.dart';
 
 class CurrencyConverterView extends StatelessWidget {
   const CurrencyConverterView({super.key});
@@ -14,7 +15,7 @@ class CurrencyConverterView extends StatelessWidget {
           child: Text("Conversor de moedas", style: AppTheme.title),
         ),
         FutureBuilder(
-          future: future,
+          future: CurrencyConverterService().getLatestCurrency(),
           builder: (context, snapshot) {
             if (snapshot.hasData &&
                 snapshot.data != null &&
