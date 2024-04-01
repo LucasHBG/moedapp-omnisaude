4eb1b932aa4bb9c4b8a21788ec94349d3e28c411
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-04-01 05:53:49 -0300

    chore: moving nav controller provider to new file

--- lib/src/home_bottom_navigation_bar_view.dart
+++ lib/src/home_bottom_navigation_bar_view.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:moedapp/src/navigation_controller_provider.dart';
 import 'package:provider/provider.dart';
-
-class NavigationControllerProvider extends ChangeNotifier {
-  int _selectedIndex = 0;
-
-  int get selectedIndex => _selectedIndex;
-
-  void setSelectedIndex(int newIndex) {
-    _selectedIndex = newIndex;
-    notifyListeners();
-  }
-}
 
 class HomeBottomNavigationBarView extends StatelessWidget {
   const HomeBottomNavigationBarView({super.key});
