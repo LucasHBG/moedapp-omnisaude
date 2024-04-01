f47ad7ebc8e99ecbbd5d18beb047984ce7362566
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-04-01 05:52:48 -0300

    feat: added new  text styles to the app

--- lib/src/constants/app_theme.dart
+++ lib/src/constants/app_theme.dart
@@ -5,30 +5,32 @@ class AppTheme {
 
   static const Color appBackgroundColor = Color(0xF2F2F2F2);
   static const Color primaryTextColor = Color(0xFF627282);
+  static const Color darkTextColor = Color(0xFF3C3E41);
+
+  static Color inputBackgroundColor = const Color(0xFFCCD6E0).withOpacity(0.2);
+  static const Color primaryColor = Color(0xFF1980C4);
 
   static const String fontFamilyName = 'OpenSans';
 
   static const TextTheme textTheme = TextTheme(
     displayLarge: h1,
-    titleLarge: title,
+    titleLarge: h1,
     titleMedium: subtitle,
     bodySmall: caption,
   );
 
   static const TextStyle h1 = TextStyle(
-    // h4 -> display1
-    fontFamily: fontFamilyName,
-    fontWeight: FontWeight.bold,
-    fontSize: 36,
-    letterSpacing: 0.4,
-    height: 0.9,
-    color: primaryTextColor,
-  );
-
-  static const TextStyle title = TextStyle(
     fontFamily: fontFamilyName,
     fontWeight: FontWeight.w800,
     fontSize: 30,
+    letterSpacing: 0,
+    color: primaryTextColor,
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w700,
+    fontSize: 25,
     letterSpacing: 0,
     color: primaryTextColor,
   );
@@ -47,5 +49,29 @@ class AppTheme {
     fontSize: 9,
     letterSpacing: 0,
     color: primaryTextColor, // was lightText
+  );
+
+  static const TextStyle inputHint = TextStyle(
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    letterSpacing: 0,
+    color: primaryTextColor,
+  );
+
+  static const TextStyle inputValue = TextStyle(
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w500,
+    fontSize: 21,
+    letterSpacing: 0,
+    color: primaryTextColor,
+  );
+
+  static const TextStyle commonText = TextStyle(
+    fontFamily: fontFamilyName,
+    fontWeight: FontWeight.w400,
+    fontSize: 19,
+    letterSpacing: 0,
+    color: darkTextColor,
   );
 }
