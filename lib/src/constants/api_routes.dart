4c20aeecd02932841baa9d17967eb291ee5f40f7
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-04-01 05:44:50 -0300

    feat: adicionado parametro amount na query

--- lib/src/constants/api_routes.dart
+++ lib/src/constants/api_routes.dart
@@ -3,6 +3,7 @@ import 'package:moedapp/src/config.dart';
 class ApiRoutes {
   static const String _baseApiUrl = Config.backendServiceEndpoint;
 
-  static String getLatestCurrency({String? fromCurrency, String? toCurrency}) =>
-      '$_baseApiUrl/latest?from=${fromCurrency ?? "BRL"}&to=${toCurrency ?? "USD"}';
+  static String getLatestCurrency(
+          {String? amount, String? fromCurrency, String? toCurrency}) =>
+      '$_baseApiUrl/latest?amount=${amount ?? "10.00"}&&from=${fromCurrency ?? "BRL"}&to=${toCurrency ?? "USD"}';
 }
